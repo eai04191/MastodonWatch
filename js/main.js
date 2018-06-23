84cb0a3309e33e72f310292f6f29d0968a9fc80d
eai04191 <eai@mizle.net>
2018-06-24 03:18:39 +0900

    lint

--- js/main.js
+++ js/main.js
@@ -1,6 +1,3 @@
-"use strict";
-
-
 function getOption() {
   const instance = "mstdn.jp";
   const token = decodeURIComponent(location.search.match(/token=(.*?)(&|$)/)[1]);
@@ -10,38 +7,38 @@ function getOption() {
   // すべてのtootを流す
   const testModeFlag = false;
   return {
-    instance: instance,
-    token: token,
-    searchRegExp: searchRegExp,
-    testModeFlag: testModeFlag
+    instance,
+    token,
+    searchRegExp,
+    testModeFlag,
   };
 }
 
 function update(text) {
-  let div = document.getElementById("articles-container");
-  let content = `<p>${text}</p>`;
-  div.innerHTML = "<article>" + content + "</article>" + div.innerHTML;
+  const div = document.getElementById("articles-container");
+  const content = `<p>${text}</p>`;
+  div.innerHTML = `<article>${content}</article>${div.innerHTML}`;
 }
 
 function connect(instance, token, searchRegExp, testModeFlag) {
-  let ws = new WebSocket(`wss://${instance}/api/v1/streaming?access_token=${token}&stream=public`);
+  const ws = new WebSocket(`wss://${instance}/api/v1/streaming?access_token=${token}&stream=public`);
 
-  ws.onopen = function () {
+  ws.onopen = function onopen() {
     update("Connected.");
   };
 
-  ws.onerror = function (error) {
+  ws.onerror = function onerror(error) {
     update("Connection Error");
     // TODO: エラーメッセージを画面に出したい
     console.log(error);
   };
 
-  ws.onmessage = function (e) {
+  ws.onmessage = function onmessage(e) {
     // console.log(JSON.parse(JSON.parse(e.data)['payload']));
-    let data = JSON.parse(e.data);
-    if (data["event"] == "update") {
-      let toot = JSON.parse(data["payload"]);
-      let text = toot["content"];
+    const data = JSON.parse(e.data);
+    if (data.event === "update") {
+      const toot = JSON.parse(data.payload);
+      const text = toot.content;
       let hit = false;
       if (searchRegExp.test(text)) {
         hit = true;
@@ -49,37 +46,17 @@ function connect(instance, token, searchRegExp, testModeFlag) {
 
       if (hit || testModeFlag) {
         console.log(toot);
-        let account_name = toot["account"]["display_name"];
-        let account_icon = toot["account"]["avatar"];
-        let account_url = toot["account"]["url"];
-        let account_instance = toot["account"]["url"].match(/https:\/\/(.*?)\//)[1];
-        let account_username = toot["account"]["username"];
-        let created_at = toot["created_at"];
-        let content = toot["content"];
-        let url = toot["url"];
-        let id = toot["id"];
-        let div = document.getElementById("articles-container");
+        const div = document.getElementById("articles-container");
 
-        let icon_html = `<a href="${account_url}" target="_blank" ><img src="${account_icon}" class="icon"></a>`;
-        let name_html =
-          `<a href="${account_url}" target="_blank" class="name">${account_name}<span class="username">${account_username}@${account_instance}</span></a>`;
-        let time_html = `<a href="https://mstdn.io/web/statuses/${id}" class="time" target="_blank">` + moment(
-          created_at).format("h:mm") + "</a>";
-        let content_html = `<div class="text">${content}</div>`;
-        div.innerHTML =
-          "<article>" +
-          "<header>" +
-          icon_html +
-          name_html +
-          time_html +
-          "</header>" +
-          content_html +
-          "</article>" +
-          div.innerHTML;
+        const iconHtml = `<a href="${toot.account.url}" target="_blank" ><img src="${toot.account.avatar}" class="icon"></a>`;
+        const nameHtml =          `<a href="${toot.account.url}" target="_blank" class="name">${toot.account.display_name}<span class="username">${toot.account.username}@${toot.account.url.match(/https:\/\/(.*?)\//)[1]}</span></a>`;
+        const timeHtml = `<a href="${toot.url}" class="time" target="_blank">${moment(toot.created_at).format("h:mm") }</a>`;
+        const contentHtml = `<div class="text">${toot.content}</div>`;
+        div.innerHTML = `<article><header>${iconHtml}${nameHtml}${timeHtml}</header>${contentHtml}</article>${div.innerHTML}`;
       }
     }
   };
 }
 
-let option = getOption();
+const option = getOption();
 connect(option.instance, option.token, option.searchRegExp, option.testModeFlag);
