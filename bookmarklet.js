13cdadbf64f62fdff43727b659e54245bc6cfcb8
eai04191 <eai@mizle.net>
2018-06-24 02:34:55 +0900

    fix url

--- bookmarklet.js
+++ bookmarklet.js
@@ -1,6 +1,6 @@
 (function () {
   window.open(
-    "https://eai04191.github.io/MastodonWatch",
+    "https://eai04191.github.io/mastodon-watch",
     "mastodonWatch",
     "width=310,height=1000"　 //widthとheight両方必要
   );
